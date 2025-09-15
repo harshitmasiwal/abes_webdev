ed9620e6faa7db0624f24b1253ae895f1da5ad87
harshitmasiwal <hmasiwal15@gmail.com>
2025-09-15 12:28:31 +0530

    day04

---
+++ day04/app.js
@@ -0,0 +1,14 @@
+const no = 3 ;
+
+function display(no){
+
+    for( i = 0 ; i <= 10 ; i=i+2){
+         console.log(no*i)
+    }
+    for( i = 1 ; i <= 10 ; i=i+2){
+         console.log(no*i)
+    }
+
+}
+
+display(no)
