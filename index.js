13a6fdaab555c55df6e4077fcb6e9788ef710d66
harshitmasiwal <hmasiwal15@gmail.com>
2026-02-11 15:40:55 +0530

    new

---
+++ index.js
@@ -0,0 +1,37 @@
+// const fs = require('fs');
+// fs.writeFile('./hello.txt', 'Hello, World!', () => {  });
+
+// fs.readFile('./hello.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// fs.appendFile('./hello.txt', '\nAppended text.', (err) => {
+//     if (err) {
+//         console.error('Error appending to file:', err);
+//         return;
+//     }
+//     console.log('Text appended successfully.');
+// });
+
+// fs.cpSync('a1.txt', 'b2.txt');
+// fs.cp('a1.txt', 'b2.txt', (err) => {
+//     if (err) {
+//         console.error('Error copying file:', err);
+//         return;
+//     }
+//     console.log('File copied successfully.');
+// });
+
+// fs.unlinkSync('./hello.txt');
+
+const os = require('os')
+console.log(os.platform())
+console.log(os.userInfo())
+console.log(os.freemem())
+console.log(os.hostname())
+
+//crreate ea minimal web app that has a 
